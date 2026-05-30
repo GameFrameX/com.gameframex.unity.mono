f0c92318c38e6c139a090c8b2411e1b72aefd785
Blank <wangfj11@foxmail.com>
2026-05-30 22:12:51 +0800

    test(mono): 重写单元测试，覆盖核心生命周期功能
    移除无关的 DateTime 测试，重写为 28 个测试覆盖：
    - MonoManager: null 参数守卫、6 种生命周期分发、Add/Remove 行为、
      异常隔离、Shutdown 清空
    - EventArgs: Create/Clear/Id 正确性

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -1,33 +1,347 @@
 using System;
+using System.Reflection;
 using NUnit.Framework;
 
 namespace GameFrameX.Mono.Tests
 {
-    internal class UnitTests
+    [TestFixture]
+    internal class MonoManagerTests
     {
-        [Test]
-        public void TestIsUnixSameDay_SameDay_ReturnsTrue()
+        private MonoManager _manager;
+
+        [SetUp]
+        public void SetUp()
         {
-            long timestamp1 = 1617842400; // April 7, 2021 12:00:00 AM UTC
-            long timestamp2 = 1617896400; // April 7, 2021 12:00:00 PM UTC
+            _manager = new MonoManager();
+        }
 
-            var date1 = DateTimeOffset.FromUnixTimeSeconds(timestamp1).UtcDateTime;
-            var date2 = DateTimeOffset.FromUnixTimeSeconds(timestamp2).UtcDateTime;
+        [TearDown]
+        public void TearDown()
+        {
+            InvokeShutdown(_manager);
+            _manager = null;
+        }
 
-            Assert.That(date1.Year, Is.EqualTo(date2.Year));
-            Assert.That(date1.Month, Is.EqualTo(date2.Month));
-            Assert.That(date1.Day, Is.EqualTo(date2.Day));
+        #region Null Argument Guard
+
+        [Test]
+        public void AddUpdateListener_Null_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => _manager.AddUpdateListener(null));
         }
 
         [Test]
-        public void TestDateTimeAddHoursSameDay()
+        public void RemoveUpdateListener_Null_Throws()
         {
-            var now = DateTime.Now;
-            var later = now.AddHours(1);
+            Assert.Throws<ArgumentNullException>(() => _manager.RemoveUpdateListener(null));
+        }
 
-            Assert.That(later.Year, Is.EqualTo(now.Year));
-            Assert.That(later.Month, Is.EqualTo(now.Month));
-            Assert.That(later.Day, Is.EqualTo(now.Day));
+        [Test]
+        public void AddFixedUpdateListener_Null_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => _manager.AddFixedUpdateListener(null));
+        }
+
+        [Test]
+        public void RemoveFixedUpdateListener_Null_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => _manager.RemoveFixedUpdateListener(null));
+        }
+
+        [Test]
+        public void AddLateUpdateListener_Null_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => _manager.AddLateUpdateListener(null));
+        }
+
+        [Test]
+        public void RemoveLateUpdateListener_Null_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => _manager.RemoveLateUpdateListener(null));
+        }
+
+        [Test]
+        public void AddDestroyListener_Null_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => _manager.AddDestroyListener(null));
+        }
+
+        [Test]
+        public void RemoveDestroyListener_Null_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => _manager.RemoveDestroyListener(null));
+        }
+
+        [Test]
+        public void AddOnApplicationFocusListener_Null_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => _manager.AddOnApplicationFocusListener(null));
+        }
+
+        [Test]
+        public void RemoveOnApplicationFocusListener_Null_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => _manager.RemoveOnApplicationFocusListener(null));
+        }
+
+        [Test]
+        public void AddOnApplicationPauseListener_Null_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => _manager.AddOnApplicationPauseListener(null));
+        }
+
+        [Test]
+        public void RemoveOnApplicationPauseListener_Null_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => _manager.RemoveOnApplicationPauseListener(null));
+        }
+
+        #endregion
+
+        #region Dispatch
+
+        [Test]
+        public void Update_DispatchesToListeners()
+        {
+            float receivedA = -1f;
+            float receivedB = -1f;
+            _manager.AddUpdateListener((a, b) =>
+            {
+                receivedA = a;
+                receivedB = b;
+            });
+
+            ((IMonoManager)_manager).Update(1.5f, 2.5f);
+
+            Assert.AreEqual(1.5f, receivedA);
+            Assert.AreEqual(2.5f, receivedB);
+        }
+
+        [Test]
+        public void FixedUpdate_DispatchesToListeners()
+        {
+            bool called = false;
+            _manager.AddFixedUpdateListener((a, b) => { called = true; });
+
+            _manager.FixedUpdate();
+
+            Assert.IsTrue(called);
+        }
+
+        [Test]
+        public void LateUpdate_DispatchesToListeners()
+        {
+            bool called = false;
+            _manager.AddLateUpdateListener((a, b) => { called = true; });
+
+            _manager.LateUpdate();
+
+            Assert.IsTrue(called);
+        }
+
+        [Test]
+        public void OnDestroy_DispatchesToListeners()
+        {
+            bool called = false;
+            _manager.AddDestroyListener(() => { called = true; });
+
+            _manager.OnDestroy();
+
+            Assert.IsTrue(called);
+        }
+
+        [Test]
+        public void OnApplicationFocus_DispatchesTrue()
+        {
+            bool? received = null;
+            _manager.AddOnApplicationFocusListener(f => { received = f; });
+
+            _manager.OnApplicationFocus(true);
+
+            Assert.IsTrue(received);
+        }
+
+        [Test]
+        public void OnApplicationFocus_DispatchesFalse()
+        {
+            bool? received = null;
+            _manager.AddOnApplicationFocusListener(f => { received = f; });
+
+            _manager.OnApplicationFocus(false);
+
+            Assert.IsFalse(received);
+        }
+
+        [Test]
+        public void OnApplicationPause_DispatchesTrue()
+        {
+            bool? received = null;
+            _manager.AddOnApplicationPauseListener(p => { received = p; });
+
+            _manager.OnApplicationPause(true);
+
+            Assert.IsTrue(received);
+        }
+
+        [Test]
+        public void OnApplicationPause_DispatchesFalse()
+        {
+            bool? received = null;
+            _manager.AddOnApplicationPauseListener(p => { received = p; });
+
+            _manager.OnApplicationPause(false);
+
+            Assert.IsFalse(received);
+        }
+
+        #endregion
+
+        #region Add/Remove Behavior
+
+        [Test]
+        public void RemoveUpdateListener_StopsDispatch()
+        {
+            int callCount = 0;
+            Action<float, float> listener = (a, b) => callCount++;
+
+            _manager.AddUpdateListener(listener);
+            ((IMonoManager)_manager).Update(1f, 1f);
+
+            _manager.RemoveUpdateListener(listener);
+            ((IMonoManager)_manager).Update(1f, 1f);
+
+            Assert.AreEqual(1, callCount);
+        }
+
+        [Test]
+        public void RemoveNonExistentListener_DoesNotThrow()
+        {
+            Assert.DoesNotThrow(() => _manager.RemoveUpdateListener((a, b) => { }));
+        }
+
+        [Test]
+        public void MultipleListeners_AllCalled()
+        {
+            int call1 = 0;
+            int call2 = 0;
+            _manager.AddUpdateListener((a, b) => call1++);
+            _manager.AddUpdateListener((a, b) => call2++);
+
+            ((IMonoManager)_manager).Update(1f, 1f);
+
+            Assert.AreEqual(1, call1);
+            Assert.AreEqual(1, call2);
+        }
+
+        [Test]
+        public void ExceptionInListener_DoesNotStopOthers()
+        {
+            bool secondCalled = false;
+            _manager.AddUpdateListener((a, b) => { throw new InvalidOperationException("test"); });
+            _manager.AddUpdateListener((a, b) => { secondCalled = true; });
+
+            ((IMonoManager)_manager).Update(1f, 1f);
+
+            Assert.IsTrue(secondCalled);
+        }
+
+        #endregion
+
+        #region Shutdown
+
+        [Test]
+        public void Shutdown_ClearsAllListeners()
+        {
+            bool called = false;
+            _manager.AddUpdateListener((a, b) => { called = true; });
+            _manager.AddDestroyListener(() => { called = true; });
+
+            InvokeShutdown(_manager);
+
+            ((IMonoManager)_manager).Update(1f, 1f);
+            _manager.OnDestroy();
+
+            Assert.IsFalse(called);
+        }
+
+        #endregion
+
+        private static void InvokeShutdown(MonoManager manager)
+        {
+            if (manager == null)
+            {
+                return;
+            }
+
+            var method = typeof(MonoManager).GetMethod("Shutdown",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            method?.Invoke(manager, null);
+        }
+    }
+
+    [TestFixture]
+    internal class OnApplicationFocusChangedEventArgsTests
+    {
+        [Test]
+        public void Create_SetsIsFocus_True()
+        {
+            var args = OnApplicationFocusChangedEventArgs.Create(true);
+            Assert.IsTrue(args.IsFocus);
+        }
+
+        [Test]
+        public void Create_SetsIsFocus_False()
+        {
+            var args = OnApplicationFocusChangedEventArgs.Create(false);
+            Assert.IsFalse(args.IsFocus);
+        }
+
+        [Test]
+        public void Clear_ResetsIsFocus()
+        {
+            var args = OnApplicationFocusChangedEventArgs.Create(true);
+            args.Clear();
+            Assert.IsFalse(args.IsFocus);
+        }
+
+        [Test]
+        public void Id_MatchesEventId()
+        {
+            var args = new OnApplicationFocusChangedEventArgs();
+            Assert.AreEqual(OnApplicationFocusChangedEventArgs.EventId, args.Id);
+        }
+    }
+
+    [TestFixture]
+    internal class OnApplicationPauseChangedEventArgsTests
+    {
+        [Test]
+        public void Create_SetsIsPause_True()
+        {
+            var args = OnApplicationPauseChangedEventArgs.Create(true);
+            Assert.IsTrue(args.IsPause);
+        }
+
+        [Test]
+        public void Create_SetsIsPause_False()
+        {
+            var args = OnApplicationPauseChangedEventArgs.Create(false);
+            Assert.IsFalse(args.IsPause);
+        }
+
+        [Test]
+        public void Clear_ResetsIsPause()
+        {
+            var args = OnApplicationPauseChangedEventArgs.Create(true);
+            args.Clear();
+            Assert.IsFalse(args.IsPause);
+        }
+
+        [Test]
+        public void Id_MatchesEventId()
+        {
+            var args = new OnApplicationPauseChangedEventArgs();
+            Assert.AreEqual(OnApplicationPauseChangedEventArgs.EventId, args.Id);
         }
     }
 }
